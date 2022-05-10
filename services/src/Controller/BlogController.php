854a0f3f16c01cdb8c4583ebe9c7b642349f6da9
Mark Yuan <mark@webasone.com>
2022-05-10 11:38:13 -0700

    greeting

--- services/src/Controller/BlogController.php
+++ services/src/Controller/BlogController.php
@@ -1,26 +1,29 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\Greeting;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController {
- private $greeting;
+class BlogController extends AbstractController
+{
+    private $greeting;
 
- public function __construct(Greeting $greeting)
- {
-  $this->greeting = $greeting;
- }
- /**
-  * @Route("/", "blog_index")
-  */
- public function index(Request $request): Response {
+    public function __construct(Greeting $greeting)
+    {
+        $this->greeting = $greeting;
+    }
+    /**
+     * @Route("/", "blog_index")
+     */
+    public function index(Request $request): Response
+    {
 
-  $name = $request->get(key:'name');
-  $message = $this->greeting->greeting($name);
+        $name = $request->get(key:'name');
+        $message = $this->greeting->greeting($name);
 
-  return $this->render('base.html.twig', ['message' => $message]);
- }
- }
+        return $this->render('base.html.twig', ['message' => $message]);
+    }
+}
