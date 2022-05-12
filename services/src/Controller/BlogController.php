b9385f860e0a4a4e1919f29bda221560ce45f5b4
Mark Yuan <mark@webasone.com>
2022-05-11 21:45:28 -0700

    Update

--- services/src/Controller/BlogController.php
+++ services/src/Controller/BlogController.php
@@ -1,29 +1,22 @@
 <?php
 namespace App\Controller;
 
-use App\Service\Greeting;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    private $greeting;
 
-    public function __construct(Greeting $greeting)
-    {
-        $this->greeting = $greeting;
-    }
     /**
      * @Route("/", "blog_index")
      */
-    public function index(Request $request): Response
+    public function index(MicroPostRepository $microRepository): Response
     {
 
-        $name = $request->get(key:'name');
-        $message = $this->greeting->greeting($name);
+        $posts = $microRepository->findAll();
 
-        return $this->render('base.html.twig', ['message' => $message]);
+        return $this->render('base.html.twig', ['posts' => $posts]);
     }
 }
